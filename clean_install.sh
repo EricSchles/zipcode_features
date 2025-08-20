4b646914e64ec5d053d8ac6957e9957f29b56a47
Eric Schles <ericschles@gmail.com>
2025-08-20 12:10:19 -0400

    first commit

---
+++ clean_install.sh
@@ -0,0 +1,5 @@
+rm -Rf /Library/Frameworks/Python.framework/Versions/3.13/lib/python3.13/site-packages/zipcode_features-0.0.1-py3.13.egg
+rm -Rf dist
+rm -Rf zipcode_features.egg-info
+rm -Rf build
+python -m pip uninstall -y zipcode_features
